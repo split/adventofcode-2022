578ed0445147a3171da0ed939c1666755654dd49
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2022-12-01 08:37:05 +0000

    day01: solutions for part1 and part2

---
+++ day01.hs
@@ -0,0 +1,8 @@
+import Data.List (sort)
+import Data.List.Split (splitOn)
+
+
+main = interact (unlines . sequence [part1, part2])
+
+part1 = ("Part 1: " ++) . show . maximum . map (sum . map read . lines) . splitOn "\n\n"
+part2 = ("Part 2: " ++) . show . sum . take 3 . reverse . sort . map (sum . map read . lines) . splitOn "\n\n"
