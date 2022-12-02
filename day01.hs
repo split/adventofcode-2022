7ad607b25768321396a9ef32ba805a6c50735746
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2022-12-02 13:24:11 +0000

    day01: formatting and module def

--- day01.hs
+++ day01.hs
@@ -1,8 +1,10 @@
+module Main where
+
 import Data.List (sort)
 import Data.List.Split (splitOn)
-
 
 main = interact (unlines . sequence [part1, part2])
 
 part1 = ("Part 1: " ++) . show . maximum . map (sum . map read . lines) . splitOn "\n\n"
+
 part2 = ("Part 2: " ++) . show . sum . take 3 . reverse . sort . map (sum . map read . lines) . splitOn "\n\n"
