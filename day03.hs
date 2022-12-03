4aaf91466c206907d655d167380444a2faaff020
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2022-12-03 11:50:08 +0000

    day03: simplify using folding and intersect

--- day03.hs
+++ day03.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char (isUpper, ord)
+import Data.List (intersect)
 import Data.List.Split (chunksOf)
 
 main = interact (unlines . sequence [part1, part2] . lines)
@@ -9,7 +10,7 @@ part1 = ("Part 1: " ++) . show . sum . map (priority . complement . (\rucksack -
 
 part2 = ("Part 2: " ++) . show . sum . map (priority . complement) . chunksOf 3
 
-complement (item : rest) = head $ filter (\item -> all (item `elem`) rest) item
+complement = head . foldr1 intersect
 
 priority :: Char -> Int
 priority item = ord item - if isUpper item then ord 'A' - 27 else ord 'a' - 1
