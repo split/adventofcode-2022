9dcba50e218d6bb9b6f8f3227845cc66b17752f3
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2022-12-03 11:41:14 +0000

    day03: all works for the normal case too

--- day03.hs
+++ day03.hs
@@ -5,11 +5,11 @@ import Data.List.Split (chunksOf)
 
 main = interact (unlines . sequence [part1, part2] . lines)
 
-part1 = ("Part 1: " ++) . show . sum . map (priority . complement any . (\rucksack -> chunksOf (length rucksack `div` 2) rucksack))
+part1 = ("Part 1: " ++) . show . sum . map (priority . complement . (\rucksack -> chunksOf (length rucksack `div` 2) rucksack))
 
-part2 = ("Part 2: " ++) . show . sum . map (priority . complement all) . chunksOf 3
+part2 = ("Part 2: " ++) . show . sum . map (priority . complement) . chunksOf 3
 
-complement rule (item : rest) = head $ filter (\item -> rule (item `elem`) rest) item
+complement (item : rest) = head $ filter (\item -> all (item `elem`) rest) item
 
 priority :: Char -> Int
 priority item = ord item - if isUpper item then ord 'A' - 27 else ord 'a' - 1
