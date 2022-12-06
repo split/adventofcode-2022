330fa46cccea7ee8b813e43d067ac02192bc7692
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2022-12-06 20:29:26 +0000

    day06: formatting and remove map

--- day06.hs
+++ day06.hs
@@ -1,8 +1,11 @@
 module Main where
-import Data.List (group, drop, tails, sort)
+
+import Data.List (drop, group, sort, tails)
 
 main = interact (unlines . sequence [part1, part2] . lines)
 
-part1 =("Part 1: "++) . show . start 4 . head
-part2 =("Part 2: "++) . show . start 14 . head
-start n  = (+n) . length . takeWhile (/=n) . map (length . group . sort . take n) . tails
+part1 = ("Part 1: " ++) . show . start 4 . head
+
+part2 = ("Part 2: " ++) . show . start 14 . head
+
+start n = (+ n) . length . takeWhile ((/= n) . length . group . sort . take n) . tails
