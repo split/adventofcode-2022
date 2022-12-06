c81e03d8b79b022fab464fdddc50db9c85078d04
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2022-12-06 20:49:37 +0000

    day06: there is only one line so just pass that

--- day06.hs
+++ day06.hs
@@ -2,10 +2,10 @@ module Main where
 
 import Data.List (drop, group, sort, tails)
 
-main = interact (unlines . sequence [part1, part2] . lines)
+main = interact (unlines . sequence [part1, part2])
 
-part1 = ("Part 1: " ++) . show . start 4 . head
+part1 = ("Part 1: " ++) . show . start 4
 
-part2 = ("Part 2: " ++) . show . start 14 . head
+part2 = ("Part 2: " ++) . show . start 14
 
 start n = (+ n) . length . takeWhile ((/= n) . length . group . sort . take n) . tails
