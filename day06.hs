b645c818ac6efbe1fd4ff45ed119bb63fb79b212
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2022-12-06 10:44:14 +0000

    day06: cleanup

--- day06.hs
+++ day06.hs
@@ -5,6 +5,4 @@ main = interact (unlines . sequence [part1, part2] . lines)
 
 part1 =("Part 1: "++) . show . start 4 . head
 part2 =("Part 2: "++) . show . start 14 . head
-
--- start :: String -> Int
 start n  = (+n) . length . takeWhile (/=n) . map (length . group . sort . take n) . tails
