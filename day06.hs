e26950670370de0be7c6680719f452415014be57
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2022-12-06 10:43:33 +0000

    day06: solutions for both parts

---
+++ day06.hs
@@ -0,0 +1,10 @@
+module Main where
+import Data.List (group, drop, tails, sort)
+
+main = interact (unlines . sequence [part1, part2] . lines)
+
+part1 =("Part 1: "++) . show . start 4 . head
+part2 =("Part 2: "++) . show . start 14 . head
+
+-- start :: String -> Int
+start n  = (+n) . length . takeWhile (/=n) . map (length . group . sort . take n) . tails
