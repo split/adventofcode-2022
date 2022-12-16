cf137ba5d52dfc92ba9a1dbcb7c01f546b7d9038
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2022-12-16 19:34:24 +0000

    day15: found the beacon

--- day15.hs
+++ day15.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Data.Char (isDigit)
-import Data.Foldable (minimumBy)
 import Data.Function (on)
 import Data.List (nub, sortBy)
 import Data.Ord (comparing)
@@ -11,17 +10,22 @@ import Debug.Trace (trace)
 
 type Point = (Int, Int)
 
-main = interact (unlines . sequence [part1] . map (t2p . parse) . lines)
+main = interact (unlines . sequence [part1, part2] . map (t2p . parse) . lines)
 
-part1 :: [(Point, Point)] -> String
-part1 = ("Part 1: " ++) . show . (ex 10 2000000 >>= scanFreq)
+part1, part2 :: [(Point, Point)] -> String
+part1 = ("Part 1: " ++) . show . (ex 10 2000000 >>= scanFreqEmptySpace)
+part2 = ("Part 2: " ++) . show . (ex 20 4000000 >>= scanAndLocateBeacon)
 
 ex e r points = if length points <= 14 then e else r
 
-scanFreq hz sensors = sum (map snd $ mergeSignals slices) - beacons
+scanFreqEmptySpace hz sensors = scanned - beacons
   where
+    scanned = sum $ map snd $ mergeSignals $ freqSlices hz sensors
     beacons = length $ nub $ filter ((== hz) . snd) $ map snd sensors
-    slices = sortBy (compare `on` (fst . fst)) $ filter ((> 0) . snd) $ map (sliceFreq hz) sensors
+
+scanAndLocateBeacon range sensors = tuningFreq $ locateBeacon $ filter ((== 2) . length) $ map (mergeSignals . (`freqSlices` sensors)) [0 .. range]
+  where
+    locateBeacon ((_ : ((x, y), _) : _) : _) = (x - 1, y)
 
 tuningFreq (x, y) = x * 4000000 + y
 
@@ -35,15 +39,16 @@ mergeSignals (p : xs) = let xs' = mergeSignals xs in uncurry (:) $ foldl go (p, 
       | x1 <= x2 && x2 <= (x1 + l1) = let p' = (s1, max l1 (x2 - x1 + l2)) in (p', xs'')
       | otherwise = (p, p2 : xs'')
 
-sliceFreq hz (sp@(sx, sy), bp@(bx, by)) = let d = dist sp bp - dist sp (sx, hz) in ((sx - d, hz), max 0 (2 * d + 1))
-
-dist (sx, sy) (bx, by) = abs (sx - bx) + abs (sy - by)
-
 --      2    1
 --     212   3
 --    21012  5
 --     212   3
 --      2    1
+freqSlices hz = sortBy (compare `on` (fst . fst)) . filter ((> 0) . snd) . map (sliceFreq hz)
+
+sliceFreq hz (sp@(sx, sy), bp@(bx, by)) = let d = dist sp bp - dist sp (sx, hz) in ((sx - d, hz), max 0 (2 * d + 1))
+
+dist (sx, sy) (bx, by) = abs (sx - bx) + abs (sy - by)
 
 parse [] = []
 parse chunk@(c : xs)
